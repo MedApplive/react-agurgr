ccf18ef5cf28a7ef177d32b5c157938909af2d6a
MedApplive <medapplive@live.co.uk>
2022-09-03 20:42:50 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -7,18 +7,25 @@ export default function App() {
       <h1 className="h1">Hello StackBlitz!</h1>
       <p>A sample page to experiment with.</p>
       <p>Looking for a link to GitHub</p>
-      <ul>
-        <li><button className="button">Nav1</button></li>
-        <li><button className="button">Nav2</button></li>
-        <li><button className="button">Nav3</button></li>
-      </ul>
+      <div>
+        <ul>
+          <li>
+            <button className="button">Nav1</button>
+          </li>
+          <li>
+            <button className="button">Nav2</button>
+          </li>
+          <li>
+            <button className="button">Nav3</button>
+          </li>
+        </ul>
+      </div>
+
       <div className="card">
-        <div> 
+        <div>
           <h1>Card here</h1>
           <div className="description">Card Description</div>
-        
         </div>
-       
       </div>
     </div>
   );
